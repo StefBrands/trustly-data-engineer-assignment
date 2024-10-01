9dee397530f229f4d52ceafd15fd0205d0188b1d
Stef Brands <stef_brands@live.nl>
2024-10-01 20:49:21 +0200

    Full implementation with mocked tests

--- smhi/smhi.py
+++ smhi/smhi.py
@@ -1,32 +1,77 @@
 import argparse
 import requests
 
+from tqdm import tqdm
+
 
 class SmhiParser:
     """
     Class to handle communication with and extract data from the SMHI Open API.
     """
-    BASE_URL = "https://opendata-download-metobs.smhi.se/api"
+    BASE_URL = "https://opendata-download-metobs.smhi.se/api/version/1.0"
 
     def __init__(self, suffix=".json"):
         self.suffix = suffix
 
     def _make_request(self, path=""):
-        r = requests.get(self.BASE_URL+self.suffix)
+        r = requests.get(self.BASE_URL+path+self.suffix)
         return r
 
     def check_connection(self):
         r = self._make_request()
         return r.status_code
 
+    
+    def list_parameters(self):
+        """Calls the parameter endpoint to list all available parameters."""
+        r = self._make_request(path="/parameter").json()
+        parameters = sorted(r['resource'], key=lambda d: int(d['key']))
+        for parameter in parameters:
+            print(f"{parameter['key']}, {parameter['title']} ({parameter['summary']})")
+
+    def temperatures(self):
+        """Return print out of stations with highest and lowest tempertaure.
+        
+        Note: because there is no available StationSet for this parameter we have to call all stations seperatedly."""
+        r = self._make_request(path="/parameter/2").json()
+        stations = r['station']
+        station_keys = [station['key'] for station in stations]
+        stations_with_temperatures = []
+        for station in tqdm(station_keys[0:50]): # Sliced to spare the SMHI API while developing.
+            try:
+                r = self._make_request(path=f"/parameter/2/station/{station}/period/latest-day/data").json()
+                stations_with_temperatures.append({'station': r['station']['name'], 'value': r['value'][0]['value']})
+            except:
+                pass
+        station_max_temp = max(stations_with_temperatures, key=lambda x:float(x['value']))
+        station_min_temp = min(stations_with_temperatures, key=lambda x:float(x['value']))
+
+        print(f"Highest temperature: {station_max_temp['station']}, {station_max_temp['value']} degrees")
+        print(f"Lowest temperature: {station_min_temp['station']}, {station_min_temp['value']} degrees")
+
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="""Script to extract data from SMHI's Open API"""
     )
+    parser.add_argument("--check-connection", action="store_true", help="Check connection to SMHI API")
     parser.add_argument("--parameters", action="store_true", help="List SMHI API parameters")
-    #args = parser.parse_args()
+    parser.add_argument("--temperatures", action="store_true", help="Show stations with the highest and lowest temperature")
+    args = parser.parse_args()
+
+    smhi_parser = SmhiParser()
+
+    if args.check_connection:
+        status_code = smhi_parser.check_connection()
+        print(status_code)
+    elif args.parameters:
+        smhi_parser.list_parameters()
+    elif args.temperatures:
+        smhi_parser.temperatures()
 
 
 if __name__ == "__main__":
     main()
+
+
--- tests/test_smhi.py
+++ tests/test_smhi.py
@@ -1,6 +1,51 @@
 from smhi.smhi import SmhiParser
-
+from unittest.mock import patch
 
 def test_check_connection():
     parser = SmhiParser()
     assert 200 == parser.check_connection()
+
+
+@patch('smhi.smhi.SmhiParser._make_request')
+def test_list_parameters_mocked(mock_make_request):
+    mock_response = {
+        'resource': [
+            {'key': '1', 'title': 'param1', 'summary': 'sum1'},
+            {'key': '2', 'title': 'param2', 'summary': 'sum2'},
+            {'key': '3', 'title': 'param3', 'summary': 'sum3'}
+        ]
+    }
+    mock_make_request.return_value.json.return_value = mock_response
+    parser = SmhiParser()
+    with patch('builtins.print') as mocked_print:
+        parser.list_parameters()
+
+        mocked_print.assert_any_call("1, param1 (sum1)")
+        mocked_print.assert_any_call("2, param2 (sum2)")
+        mocked_print.assert_any_call("3, param3 (sum3)")
+
+@patch('smhi.smhi.SmhiParser._make_request')
+def test_temperatures_mocked(mock_make_request):
+    mock_response_stations = {
+        'station': [
+            {'key': 'station_1', 'name': 'Station One'},
+            {'key': 'station_2', 'name': 'Station Two'},
+            {'key': 'station_3', 'name': 'Station Three'}
+        ]
+    }
+    mock_make_request.return_value.json.side_effect = [mock_response_stations,
+                                                    {'station': {'name': 'Station One'}, 'value': [{'value': '1.1'}]},
+                                                    {'station': {'name': 'Station Two'}, 'value': [{'value': '-1.1'}]},
+                                                    {'station': {'name': 'Station Three'}, 'value': [{'value': '-1.2'}]}]
+    parser = SmhiParser()
+    with patch('builtins.print') as mocked_print:
+        parser.temperatures()
+
+        mocked_print.assert_any_call("Highest temperature: Station One, 1.1 degrees")
+        mocked_print.assert_any_call("Lowest temperature: Station Three, -1.2 degrees")
+
+def test_temperatures(capsys):
+    parser = SmhiParser()
+    parser.temperatures()
+    captured = capsys.readouterr()
+    assert captured.out.strip()[0:21] == "Highest temperature: "
